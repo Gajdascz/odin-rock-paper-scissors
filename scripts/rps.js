0c52349a20a7f13b9585d735d226be565050c743
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-09-28 19:52:01 -0400

    Create getPlayerChoice function

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,14 +1,26 @@
 // The Odin Project: Rock Paper Scissors JavaScript Game
 
-
-const choices = ["rock", "paper", "scissors"]; // Set choices constant as there's only ever three options
-
 // Randomly generate a number within the length of the choices choices array
 // and return the value from the randomly generated index of the choices array.
 function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
   let computerChoice = Math.floor(Math.random()*choices.length);
   return choices[computerChoice];
 }
+
+
+// Get player choice
+function getPlayerChoice() {
+  let playerChoice = (prompt("Enter your choice: ")).toLowerCase();
+  if( (playerChoice == "rock") || (playerChoice == "paper") || (playerChoice == "scissors")){
+    return playerChoice;
+  } else { 
+    alert("Invalid Input! Please enter Rock, Paper, or Scissors"); 
+    getPlayerChoice();
+  }
+}
+
 console.log(getComputerChoice());
+getPlayerChoice();
 // Get user choice
 // Compare and decide winner  
