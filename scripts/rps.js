10fcd7f85543dccabeb3e6093064b6c15f367825
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-09-29 17:23:38 -0400

    Create game function
    Game function plays 5 rounds of rock paper scissors, records score,
    and announces a winner at the end.

--- scripts/rps.js
+++ scripts/rps.js
@@ -8,10 +8,12 @@ function getComputerChoice() {
   return choices[computerChoice];
 }
 
-
 // Get, validate, and return player choice 
 function getPlayerChoice() {
-  let playerChoice = (prompt("Enter your choice: ")).toLowerCase();
+  let playerChoice = (prompt("Enter your choice: "));
+  if (playerChoice == null){
+    throw new Error("Player Canceled. Game Terminated.");
+  } else { playerChoice = playerChoice.toLowerCase(); }
   if( (playerChoice == "rock") || (playerChoice == "paper") || (playerChoice == "scissors")){
     return playerChoice;
   } else { 
@@ -30,20 +32,45 @@ function choiceCompare(playerChoice, computerChoice) {
       (playerChoice == "scissors" && computerChoice == "paper")
     ) { return `You Win! ${playerChoice} beats ${computerChoice}`; }
     else if (
-      (playerChoice == "rock" && computerChoice == "paper") ||
-      (playerChoice == "paper" && computerChoice == "scissors") ||
-      (playerChoice == "scissors" && computerChoice == "rock")
+      (computerChoice == "rock" && playerChoice == "scissors") ||
+      (computerChoice == "paper" && playerChoice == "rock") ||
+      (computerChoice == "scissors" && playerChoice == "paper")
     ) {return `You Lose! ${computerChoice} beats ${playerChoice}`;}
-    else { console.log("ERROR! Something wrong in choiceCompare2 logic");}
+    else { console.log("ERROR! Something wrong in choiceCompare() logic");}
 }
 
-
+// get and return round result
 function playRound() {
   let playerChoice = getPlayerChoice();
   let computerChoice = getComputerChoice();
-  let roundResult = choiceCompare(playerChoice, computerChoice)
+  let roundResult = choiceCompare(playerChoice, computerChoice);
   console.log(roundResult);
+  return roundResult;
 }
 
-playRound();
-// Compare and decide winner  
+// Play a 5 round game that keeps score and reports a winner or loser at the end
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let tiedRounds = 0;
+  let round = 1;
+  while(round <= 5) {
+    let roundResult = playRound();
+    if(roundResult.includes("You Win!")) {
+      playerScore++;
+      round++;
+    } else if (roundResult.includes("You Lose!")) {
+      computerScore++;
+      round++;
+    } else {
+      tiedRounds++;
+    }
+  }
+  if(playerScore > computerScore) {
+    console.log(`Player Wins!\nPlayer Score: ${playerScore} Computer Score: ${computerScore} Tied Rounds: ${tiedRounds}`);
+  } else {
+    console.log(`Computer Wins!\nPlayer Score: ${playerScore} Computer Score: ${computerScore} Tied Rounds: ${tiedRounds}`);
+  }
+}
+
+game();
